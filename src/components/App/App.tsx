d323a2acabcdbb4150ac4633a12a104884d82c36
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2024-10-22 18:35:12 +0200

    Implement grid layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,17 @@
 import styles from "./App.module.scss";
 
 export const App = () => {
-  return <div className={styles.container}>Hello world!</div>;
+  return (
+    <main className={styles.container}>
+      <header className={styles.header}>header</header>
+
+      <nav className={styles.search}>search</nav>
+
+      <div className={styles.current}>current</div>
+
+      <div className={styles.forecast}>forecast</div>
+
+      <div className={styles.chart}>chart</div>
+    </main>
+  );
 };
