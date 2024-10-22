06bdb6bb0164d05968336ab2283f2c658d791849
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2024-10-22 18:13:23 +0200

    Scaffold the app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: {
+        exportType: "default",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
+  ],
+  css: {
+    modules: {
+      generateScopedName: "[name] - [local] - [hash:base64:5]",
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "/src/styles/config.scss" as *;`,
+      },
+    },
+  },
+});
