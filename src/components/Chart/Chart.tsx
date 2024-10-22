4119854fd40328ce9543e4b2d24497164bfd51cd
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2024-10-22 23:19:24 +0200

    Add charts

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,5 +1,43 @@
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+import { useForecasts } from "../../hooks/use-forecasts";
 import styles from "./Chart.module.scss";
 
 export const Chart = () => {
-  return <div className={styles.container}>Chart</div>;
+  const { forecast } = useForecasts();
+
+  return (
+    <div className={styles.container}>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart width={500} height={300} data={forecast}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="day" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="min"
+            stroke="#1e88e5"
+            strokeWidth={3}
+          />
+          <Line
+            type="monotone"
+            dataKey="max"
+            stroke="#f48403"
+            strokeWidth={3}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
