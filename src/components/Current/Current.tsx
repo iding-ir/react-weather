7ffe81ee2392b3771c03f5328489102cfb9beaa7
Aydin Ghane Kh. <aydin.ghane.kh@gmail.com>
2024-10-22 19:13:16 +0200

    Populate Current component

--- src/components/Current/Current.tsx
+++ src/components/Current/Current.tsx
@@ -1,5 +1,15 @@
+import clsx from "clsx";
+
 import styles from "./Current.module.scss";
 
 export const Current = () => {
-  return <div className={styles.container}>Current</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.location}>Amsterdam, NL</div>
+      <div className={styles.temperature}>7℃</div>
+      <div className={clsx(styles.switch, styles.centigrade)}>C</div>
+      <div className={clsx(styles.switch, styles.fahrenheit)}>F</div>
+      <div className={styles.description}>Thursday, light rain</div>
+    </div>
+  );
 };
